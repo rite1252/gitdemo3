b210a1afcce78e529ff4aa4e53eeb7ccf774bd8c
rite1252 <ritesh.bisht4141@gmail.com>
2022-05-12 07:58:00 +0530

    1st commit

---
+++ src/gitdemo2/HelloIndia.java
@@ -0,0 +1,9 @@
+package gitdemo2;
+
+public class HelloIndia {
+
+	public static void main(String[] args) {
+		System.out.println("Hello India");
+	}
+
+}
---
+++ src/gitdemo3/HelloDelhi.java
@@ -0,0 +1,10 @@
+package gitdemo3;
+
+public class HelloDelhi {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Delhi");
+
+	}
+
+}
